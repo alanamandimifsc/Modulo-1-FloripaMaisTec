4218db694f9285650b62494eadd398c25da765ba
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-02-25 18:34:03 -0300

    Exercicio 7

---
+++ Semana 06/Exercicio 7/script.js
@@ -0,0 +1,7 @@
+// Crie um arquivo JavaScript chamado script.js e adicione uma função simples que escreva uma mensagem de boas-vindas no alert.
+
+function welcome() {
+    alert("Bem-vindo ao meu site!");
+}
+
+welcome();
