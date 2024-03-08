73f9e9a9cf41769b2362b9d246629d6ecf5f3fb7
Alana IFSC <alana.brm@aluno.ifsc.edu.br>
2024-03-08 14:29:30 -0300

    Exercicio 2

--- Semana08/script.js
+++ Semana08/script.js
@@ -2,3 +2,10 @@
 
 var elemento = document.getElementById('title');
 elemento.style.backgroundColor = 'red';
+
+//selecione elementos por classe
+var elementos = document.querySelectorAll('.item');
+
+for (var i = 0; i < elementos.length; i++) {
+    elementos[i].textContent = 'Mudando item ' + i;
+}
